3aa91173cdc2bec81a36345429fd413519223c30
ruizhong123 <ruizhongzhang310@gmail.com>
2025-04-04 21:41:13 +0800

    video_straming_can_activate

--- APP/urls.py
+++ APP/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main_view, name='main_view'),
-    path('stream/', views.stream_video_counting_region, name='stream_video_counting_region'),
+    path('stream/', views.stream_video, name='stream_video'),
     path('charts/data/', views.get_chart_data, name='get_chart_data'),
 ]
